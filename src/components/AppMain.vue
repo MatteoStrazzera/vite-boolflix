7316b92501d0ea7aa6fb8fb0199c70f79f62bc2c
MatteoStrazzera <mattestrazz@gmail.com>
2024-04-05 18:41:08 +0200

    add movies_sec and series_sec

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,19 +21,29 @@ export default {
 
 <template>
     <div id="site_main">
-        <ul>
-            <li v-for="movie in state.movies">
-                <div class="title">{{ movie.title }}</div>
-                <div class="original_title">{{ movie.original_title }}</div>
-                <div class="language" v-if="getFlags(movie.original_language)">
-                    <img width="20" :src="'../public/images/' + movie.original_language + '.png'" alt="">
-                </div>
-                <div v-else>
-                    Language: {{ movie.original_language }}
-                </div>
-                <div class="rating">{{ movie.vote_average }}</div>
-            </li>
-        </ul>
+        <div class="movies_section">
+            <ul>
+                <li v-for="movie in state.movies">
+                    <div class="title">{{ movie.title }}</div>
+                    <div class="original_title">{{ movie.original_title }}</div>
+                    <div class="language" v-if="getFlags(movie.original_language)">
+                        <img width="20" :src="'../public/images/' + movie.original_language + '.png'" alt="">
+                    </div>
+                    <div v-else>
+                        Language: {{ movie.original_language }}
+                    </div>
+                    <div class="rating">{{ movie.vote_average }}</div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="series_section">
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 

8cd0960e6fbb6ec7475c25d6de7f5357781b6264
MatteoStrazzera <mattestrazz@gmail.com>
2024-04-05 18:33:27 +0200

    add methods and v-if to get flags image

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,7 +4,16 @@ export default {
     name: "AppMain",
     data() {
         return {
-            state
+            state,
+            languages: ['it', 'en', 'de', 'fr', 'sp', 'us']
+        }
+    },
+    methods: {
+        getFlags(lang) {
+            if (this.languages.includes(lang)) {
+                return true;
+            }
+            return false;
         }
     }
 }
@@ -16,7 +25,12 @@ export default {
             <li v-for="movie in state.movies">
                 <div class="title">{{ movie.title }}</div>
                 <div class="original_title">{{ movie.original_title }}</div>
-                <div class="language">{{ movie.original_language }}</div>
+                <div class="language" v-if="getFlags(movie.original_language)">
+                    <img width="20" :src="'../public/images/' + movie.original_language + '.png'" alt="">
+                </div>
+                <div v-else>
+                    Language: {{ movie.original_language }}
+                </div>
                 <div class="rating">{{ movie.vote_average }}</div>
             </li>
         </ul>
